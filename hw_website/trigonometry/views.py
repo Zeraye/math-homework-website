88d036ba0c0927917a349ba922711ec98c5fe6df
Jakub Rudnik <jakubrudnik02@gmail.com>
2020-09-26 19:33:34 +0200

    commit

--- hw_website/trigonometry/views.py
+++ hw_website/trigonometry/views.py
@@ -4,38 +4,77 @@ from .forms import TrigonometryForm
 import math
 
 
-def trigonometry_solver(arr):
-    # code goes here
-    [angle, sin, cos, tan, cot] = arr
-    angle = arr[0]
-    print(type(angle))
-    if angle != "":
-        sin = math.sin(math.radians(angle))
-        cos = math.cos(math.radians(angle))
-        tan = math.tan(math.radians(angle))
-        if tan != 0: cot = 1/t
-        else: cot = "nie istnieje"
-    else:
-        return "1", "1", "1", "1", "1"
-    return str(angle), str(sin), str(cos),  str(tan),  str(cot)
+def trigonometry_solver(angle, sin, cos, tan, cot):
+    approx = 3
+    try:
+        if angle != "":
+            angle = math.radians(float(angle))
+            sin = math.sin(angle)
+            cos = math.cos(angle)
+            tan = math.tan(angle)
+            if tan != 0: cot = 1/tan
+            else: cot = "brak"
+        elif sin != "":
+            sin = float(sin)
+            angle = math.asin(sin)
+            cos = math.cos(angle)
+            tan = math.tan(angle)
+            if tan != 0: cot = 1/tan
+            else: cot = "brak"
+        elif cos != "":
+            cos = float(cos)
+            angle = math.acos(sin)
+            sin = math.sin(angle)
+            tan = math.tan(angle)
+            if tan != 0: cot = 1/tan
+            else: cot = "brak"
+        elif tan != "":
+            tan = float(tan)
+            angle = math.atan(tan)
+            sin = math.sin(angle)
+            cos = math.cos(angle)
+            if tan != 0: cot = 1/tan
+            else: cot = "brak"
+        elif cot != "":
+            cot = float(cot)
+            if cot != 0:
+                tan = 1/cot
+                angle = math.atan(tan)
+                sin = math.sin(angle)
+                cos = math.cos(angle)
+            else:
+                tan = "brak"
+                cos = 0
+                sin = 1
+                angle = math.asin(sin)
+        else: return "brak", "brak", "brak", "brak", "brak"
+        if sin != "brak" and sin > 1000: sin = "&infin;"
+        if cos != "brak" and cos > 1000: cos = "&infin;"
+        if tan != "brak" and tan > 1000: tan = "&infin;"
+        if cot != "brak" and cot > 1000: cot = "&infin;"
+        if sin != "brak" and sin < -1000: sin = "-&infin;"
+        if cos != "brak" and cos < -1000: cos = "-&infin;"
+        if tan != "brak" and tan < -1000: tan = "-&infin;"
+        if cot != "brak" and cot < -1000: cot = "-&infin;"
+        angle = math.degrees(angle)
+        if angle != "brak": angle = round(angle, approx)
+        if sin != "brak": sin = round(sin, approx)
+        if cos != "brak": cos = round(sin, approx)
+        if tan != "brak": tan = round(sin, approx)
+        if cot != "brak": cot = round(sin, approx)
+        return str(angle), str(sin), str(cos), str(tan), str(cot)
+    except: return "brak", "brak", "brak", "brak", "brak"
 
 
 def trigonometry(request):
     if request.method == 'POST':
         form = TrigonometryForm(request.POST)
-        # code goes here
         angle = form['angle'].value()
         sin = form['sin'].value()
         cos = form['cos'].value()
         tan = form['tan'].value()
         cot = form['cot'].value()
-        arr = [angle, sin, cos, tan, cot]
-        for element in arr:
-            if element != "":
-                element = float(element)
-                print(type(element))
-                print(element)
-        list = trigonometry_solver(arr)
+        list = trigonometry_solver(angle, sin, cos, tan, cot)
         context = {
             'angle': '<p>&alpha; = ' + list[0] + '</p>',
             'sin': '<p>sin&alpha; = ' + list[1] + '</p>',
